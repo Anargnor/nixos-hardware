e96b5495dbaf97023737e1cf880db66721685210
Anargnor <135808131+Anargnor@users.noreply.github.com>
2024-12-13 22:12:04 +0400

    Fix bus ids

--- asus/zephyrus/ga605/default.nix
+++ asus/zephyrus/ga605/default.nix
@@ -11,8 +11,8 @@
 
   hardware.nvidia = {
     prime = {
-      intelBusId = "PCI:0:2:0";
-      nvidiaBusId = "PCI:1:0:0";
+      amdgpuBusId = "PCI:101:0:0";
+      nvidiaBusId = "PCI:100:0:0";
     };
 
     dynamicBoost.enable = lib.mkDefault true;
